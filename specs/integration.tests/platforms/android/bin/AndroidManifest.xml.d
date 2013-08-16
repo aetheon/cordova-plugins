85875306afa8a026297c00e94e5eb3250d297956
Oscar Brito <aetheon@gmail.com>
2013-08-16 15:53:25 +0000

    Ignore android integration test files

--- specs/integration.tests/platforms/android/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\Workspace\Projects\sps\CatalystMobile.latest\trunk\cordova-plugins\specs\integration.tests\platforms\android\bin\AndroidManifest.xml : \
-C:\Workspace\Projects\sps\CatalystMobile.latest\trunk\cordova-plugins\specs\integration.tests\platforms\android\AndroidManifest.xml \
